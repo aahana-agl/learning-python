85a4bc79d1ea5a44636053cf2515c3cae52b2989
aahana-agl <aahana.agl@gmail.com>
2026-05-19 04:21:07 +0000

    commit

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello")
+#printing commands
+#printing a greeting card
+a=("hey everyone")
+print(a)
+print(type(a))
+print()
+#printing a number
+c=1
+print(c)
+print(type(c))
+#printing a line 
+print("the sum of 3 and 5",3+5)
+print()
+print()
+print("I like playing all kinds of sport")
