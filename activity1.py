9ba2f180fc9f0298111fc6d1690acd73d2a26442
aahana-agl <aahana.agl@gmail.com>
2026-05-26 04:58:51 +0000

    lesson2

---
+++ activity1.py
@@ -0,0 +1,7 @@
+#print a statement
+print("welcome to the world of coding")
+#print a num
+print(4)
+#print a comment
+print("hello python\n")
+print("hi aahana")
---
+++ activity2.py
@@ -0,0 +1,6 @@
+X=10
+Y=30
+NAME='Aahana'
+print(X)
+print(Y)
+print(NAME)
