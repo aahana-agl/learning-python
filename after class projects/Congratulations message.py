8c2cb3f8e50fa42a7202d45074aa1f58a03a1f2d
aahana-agl <aahana.agl@gmail.com>
2026-06-08 06:14:54 +0000

    congulations messege

---
+++ after class projects/Congratulations message.py
@@ -0,0 +1,9 @@
+a="Congratulations"
+print(a[2])
+print(a[5])
+print(a[5:10:2])
+print(a[1:5])
+print(a[0:10:2])
+# I just learnt this from somewhere so I just wanted to show you this  
+a = input("Enter the word: ")
+print(a.upper())
