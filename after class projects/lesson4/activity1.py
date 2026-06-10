5a2182495e86154b0007d9575266570db6fb33bc
aahana-agl <aahana.agl@gmail.com>
2026-06-10 05:00:23 +0000

    hi

---
+++ after class projects/lesson4/activity1.py
@@ -0,0 +1,8 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+sum1=tree1+tree2+tree3+tree4+tree5
+avg=sum1/5
+print("average trees in Jacks yard:",int(avg))
---
+++ after class projects/lesson4/activity2.py
@@ -0,0 +1,8 @@
+a=12
+b=34
+#modules number
+c=a%b
+#floor division
+d=a//c
+print(d,"\n")
+print(c)
