b618b0469c05ca8bf90533c9b418332f2754297b
aahana-agl <aahana.agl@gmail.com>
2026-08-07 07:56:16 +0000

    More about Data types

---
+++ after class projects/lesson 3.3/activity1.py
@@ -0,0 +1,19 @@
+name = input("enter your name,agent: ")
+gadget = input("enter your favorite gadget: ")
+
+agent_number = 7
+speed_rating = 8.5
+mission_count = 9
+hight_m = 2.3
+is_active = False
+
+print(type(name))
+print(type(gadget))
+print(type(agent_number))
+print(type(speed_rating))
+print(type(mission_count))
+print(type(hight_m))
+print(type(is_active))
+
+agent_number_tc = str(agent_number)
+print(type(agent_number_tc))
