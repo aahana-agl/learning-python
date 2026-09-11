1178a55060966f15c3c45642bf2b5771e3808516
aahana-agl <aahana.agl@gmail.com>
2026-09-11 08:29:10 +0000

    python operators

---
+++ after class projects/lesson6.1/activity6.1.py
@@ -0,0 +1,10 @@
+a=[1,2,4]
+b=[5,6,1]
+c=a
+print(id(a))
+print(id(b))
+print(id(c))
+print(a is b)
+print(c is a)
+print(a is not b)
+
---
+++ after class projects/lesson6.1/activity6.2.py
@@ -0,0 +1,5 @@
+a=[1,2,3,"watermellon,'coconut"]
+b=["grape","mango","papaya","tangerine"]
+print("watermellon" in a )
+print("mango" in a )
+print("tangerine" in b )
