b88d38a579d407dc6ee61f4ef7ecb899905c40dc
aahana-agl <aahana.agl@gmail.com>
2026-09-17 08:20:50 +0000

    python challenges!

---
+++ lesson 6/divisible.py
@@ -0,0 +1,6 @@
+numerator = int(input("Enter a number:"))
+denominator = int(input("Enter another number:"))
+if numerator%denominator==0:
+    print("They are divisible by each other")
+else:
+    print("They arent divisible by each other")
